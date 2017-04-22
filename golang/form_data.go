e7222d8fdb7ee26efd4704ca5570229f1937c6fd
Edward Pie <hackstockpie@gmail.com>
2017-04-22 21:52:15 +0000

    Change : Demonstrates how to read individual key-value pairs from request body

--- golang/form_data.go
+++ golang/form_data.go
@@ -12,13 +12,14 @@ func main() {
 		//NOTE : Invoke ParseForm or ParseMultipartForm before reading form values
 		r.ParseForm()
 		/*
-			Loops through r.Form object to print key-value pairs of
-			url-form encoded data. Note that these include both data sent
+			Reads individual key-value pairs from
+			r.Form object. Note that these include both data sent
 			through request url and request body
 		*/
-		for key, value := range r.Form {
-			fmt.Printf("%s = %s\n", key, value)
-		}
+		fmt.Printf("USERNAME => %s\n", r.FormValue("username"))
+		fmt.Printf("EMAIL => %s\n", r.FormValue("email"))
+		fmt.Printf("PASSWORD => %s\n", r.FormValue("password"))
+		fmt.Printf("PAGEINDEX => %s\n", r.FormValue("pageindex"))
 
 		fmt.Fprintln(w, "Registeration successful. You inner man is now aligned with nature")
 	})
